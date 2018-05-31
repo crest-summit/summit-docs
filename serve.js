7cf00c27cf272de5bf8d8cf2abd0f5bb8d4d64a7
Michael Irwin <mikesir87@gmail.com>
2018-05-31 15:34:47 -0400

    Added CORS support for docs image

--- serve.js
+++ serve.js
@@ -1,9 +1,11 @@
 const express   = require("express");
+const cors = require("cors");
 
 if (process.argv[1] === __filename) {
   console.log("Starting up server");
 
   const app = express();
+  app.use(cors());
   app.use(express.static("site"));
   app.listen(80, function() {
     console.log("Listening on port 80");
