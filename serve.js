f0aa07363834666e9861d0c6fbe044e5c3c94c41
Michael Irwin <mikesir87@gmail.com>
2019-01-31 11:30:05 -0500

    Allow overriding of listening port
    CREST-6295

--- serve.js
+++ serve.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 if (process.argv[1] === __filename) {
   console.log("Starting up server");
 
+  const PORT = process.env.PORT || 80;
+
   const app = express();
   app.use(cors());
   app.use(express.static("site"));
-  app.listen(80, function() {
-    console.log("Listening on port 80");
+  app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`);
   });
 }
